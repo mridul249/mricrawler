f5b100e4b82843f74a32ce08a3ac8bd1c1f7ffbb
mridul_249 <mridul60214@gmail.com>
2026-07-14 01:01:15 +0530

    fix: update analytics routes to bypass adblockers

--- src/routes/tracking.js
+++ src/routes/tracking.js
@@ -4,7 +4,7 @@ const { trackVisit, trackEvent } = require('../controllers/trackingController');
 
 const router = express.Router();
 
-router.post('/track', verifySecret, trackVisit);
-router.post('/event', verifySecret, trackEvent);
+router.post('/ping', verifySecret, trackVisit);
+router.post('/action', verifySecret, trackEvent);
 
 module.exports = router;
